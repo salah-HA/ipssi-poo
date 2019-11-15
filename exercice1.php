329043eda9d18b8e0488e9c0fb50e4bd7d0d8eee
123456789 <s.hadj-ali@ecole-ipssi.net>
2019-11-15 16:25:00 +0100

    reponse

--- exercice1.php
+++ exercice1.php
@@ -3,21 +3,76 @@
 require_once('vendor/autoload.php');
 
 $climate = new League\CLImate\CLImate;
+use Ipssi\Evaluation\Exception\ExceptionOutOfLimit;
+use Ipssi\Evaluation\Exception\ExceptionVide;
+use Ipssi\Evaluation\Exception\ExceptionDivisionPar0;
+use Ipssi\Evaluation\Exception\ExceptionNoNumeric;
+
+
 
 class Diviseur {
     public function division(int $index, int $diviseur)
     {
-        $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
-
+        try{
+            $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
+            if ($diviseur === 0){
+                throw new ExceptionDivisionPar0('toz');
+            }
+            if ($index <0 || $index > sizeof($valeurs)){
+                throw new ExceptionOutOfLimit();
+            }
+        }
+        catch (ExceptionDivisionPar0 $e){
+            echo 'toz';
+        }
+        catch (ExceptionOutOfLimit $e){
+            echo 'en dehors du champs d action';
+        }
         return $valeurs[$index] / $diviseur;
+    }
+    public function getLength(){
+        $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
+        return sizeof($valeurs);
     }
 }
 
-$input = $climate->input("Entrez l’indice de l’entier à diviser : ");
-$index = $input->prompt();
 
-$input = $climate->input("Entrez le diviseur : ");
-$diviseur = $input->prompt();
+$repeat = false;
+while ($repeat === false){
+    try{
+        $input = $climate->input("Entrez l’indice de l’entier à diviser : ");
+        $index = $input->prompt();
+        $input = $climate->input("Entrez le diviseur : ");
+        $diviseur = $input->prompt();
 
-$climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+        if ($index < 0 || $index > 9){
+            throw new ExceptionOutOfLimit("La valeur doit être compris entre 0 et 9" . PHP_EOL . "");
+        }/*
+        if ($diviseur = 0){
+            throw new ExceptionDivisionPar0("Impossible de diviser par 0" . PHP_EOL . "");
+        }
+        /*if (empty($index) || empty($diviseur)){
+            throw new ExceptionVide("Valeur non Saisi");
+        }*/
+        if (!is_numeric($index) || !is_numeric($diviseur)){
+            throw new ExceptionNoNumeric("Valeur Saisi Incorrect");
+        }
+
+    }
+    catch (ExceptionOutOfLimit $e){
+        echo 'La valeur est en dehors du champs d action'. PHP_EOL . '' .$e->getMessage() ;
+        $repeat === false;
+    }/*
+    catch (ExceptionDivisionPar0 $e){
+        echo 'La valeur est en dehors du champs d action'. PHP_EOL . '' .$e->getMessage() ;
+    }
+    /*catch (ExceptionVide $e){
+        echo 'La valeur est en dehors du champs d action 1'. PHP_EOL . '' .$e->getMessage() ;
+    }*/
+    catch (ExceptionNoNumeric $e){
+        echo 'La valeur est en dehors du champs d action'. PHP_EOL . '' .$e->getMessage() ;
+        $repeat === false;
+    }
+    $climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+}
 
